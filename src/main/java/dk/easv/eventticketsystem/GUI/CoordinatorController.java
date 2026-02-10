0d773f6399bb5764e46c963b0ab75c05122cbe77
Frederik Ehmsen <Freehm01@easv365.dk>
2026-02-10 15:00:14 +0100

    added button actionevents and fixed the design of the CoordinatorView.fxml

--- src/main/java/dk/easv/eventticketsystem/GUI/CoordinatorController.java
+++ src/main/java/dk/easv/eventticketsystem/GUI/CoordinatorController.java
@@ -1,4 +1,36 @@
 package dk.easv.eventticketsystem.GUI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CoordinatorController {
+    @FXML
+    private TableView tblEvents;
+    @FXML
+    private TableColumn colName;
+    @FXML
+    private TableColumn colPlace;
+    @FXML
+    private TableColumn colDate;
+    @FXML
+    private TableColumn colTime;
+    @FXML
+    private TableColumn colTotalTickets;
+    @FXML
+    private TableColumn colDescription;
+
+    @FXML
+    private void handleCreateEvent(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void handleRemoveEvent(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void handleLogout(ActionEvent actionEvent) {
+
+    }
 }
