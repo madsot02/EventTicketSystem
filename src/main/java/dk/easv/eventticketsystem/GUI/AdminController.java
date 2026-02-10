bf4f770da929300ca6a5fbc7c22392dc244f8c6f
Zilas Larsen <zilas04@gmail.com>
2026-02-10 15:26:03 +0100

    Fixed AdminView.fxml

--- src/main/java/dk/easv/eventticketsystem/GUI/AdminController.java
+++ src/main/java/dk/easv/eventticketsystem/GUI/AdminController.java
@@ -4,12 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AdminController {
+
+    @FXML private TableColumn colName;
+    @FXML private TableColumn colEmail;
+    @FXML private TableColumn colRole;
+    @FXML private TableView tblAdmin;
 
     @FXML
     private void handleAddUserAdmin(ActionEvent actionEvent) throws IOException {
@@ -22,5 +29,13 @@ public class AdminController {
 
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.show();
+    }
+
+    @FXML
+    private void handleLogout(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void handleAddUser(ActionEvent actionEvent) {
     }
 }
